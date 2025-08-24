8235b2d89e536ae824774518dbb7d0a733ba33ca
Saw Simon Linn <simonlinn2@gmail.com>
2025-08-23 22:45:31 -0700

    adding google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "AboutTime Cafe",
@@ -12,6 +13,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GA_ID = "G-R35FQ191B5"; // replace with your GA tag
+
   return (
     <html lang="en">
       <head>
@@ -25,6 +28,20 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
           rel="stylesheet"
         />
+
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', { send_page_view: false });
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { AboutSection } from '@/components/landing/about';
-import { FaqSection } from '@/components/landing/faq';
-import { Footer } from '@/components/landing/footer';
-import { GallerySection } from '@/components/landing/gallery';
-import { Header } from '@/components/landing/header';
-import { HeroSection } from '@/components/landing/hero';
-import { ReelsSection } from '@/components/landing/reels';
-import { ReviewsSection } from '@/components/landing/reviews';
-import { WorkStudySection } from '@/components/landing/work-study';
+import { AboutSection } from "@/components/landing/about";
+import { FaqSection } from "@/components/landing/faq";
+import { Footer } from "@/components/landing/footer";
+import { GallerySection } from "@/components/landing/gallery";
+import { Header } from "@/components/landing/header";
+import { HeroSection } from "@/components/landing/hero";
+import { ReelsSection } from "@/components/landing/reels";
+import { ReviewsSection } from "@/components/landing/reviews";
+import { WorkStudySection } from "@/components/landing/work-study";
 
 export default function Home() {
   return (
